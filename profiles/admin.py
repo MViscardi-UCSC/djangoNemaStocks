53264c0bcb75832769f57ca697eb005267e5af68
MViscardi-UCSC <mviscard@ucsc.edu>
2024-01-10 10:59:07 -0800

    Extending user profile admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -7,4 +7,10 @@ from .models import UserProfile
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role', 'initials')
     list_filter = ('role', 'initials')
-    search_fields = ('user', 'role', 'initials')
+    search_fields = ('user__username', 'role', 'initials')
+    
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super(UserProfileAdmin, self).get_readonly_fields(request, obj)
+        if not request.user.is_superuser:
+            readonly_fields += ('user',)
+        return readonly_fields
