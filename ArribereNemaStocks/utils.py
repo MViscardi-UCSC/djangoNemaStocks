7d73ef1b127325c5c20cf07f4a9472e7fb5b7521
MViscardi-UCSC <mviscard@ucsc.edu>
2024-10-08 14:42:39 -0700

    Major re-write of bulk entry code cleaning to handle extra (or missing) columns more elegantly/safely

--- ArribereNemaStocks/utils.py
+++ ArribereNemaStocks/utils.py
@@ -1,37 +1,51 @@
 from pprint import pprint
 
 
-def parse_strain_data(data, separator='\t'):
+def parse_strain_data(data, separator='\t', number_of_fields=6,
+                      field_names=('wja', 'genotype', 'phenotype', 'source', 'description', 'additional_comments'),
+                      debug=False):
     """
     Parses the pasted data and returns a list of dictionaries suitable for initializing forms.
     Assumes data is tab-separated without headers.
     """
-    # print(f"Input Data:\n\n{data}\n")
+    assert number_of_fields == len(field_names), "Number of fields must match number of field names"
+    assert 'wja' in field_names, "WJA field must be present"
     parsed_data = []
     lines = data.strip().split('\n')
     for line in lines:
-        # print(f"Line:\n\n{line}")
-        fields = line.strip().split(separator, 5)  # 5 splits makes 6 fields?
+        fields = line.strip().split(separator)
+        if debug:
+            print("Fields:", fields)
+        
         # Handle missing fields
-        fields += [''] * (6 - len(fields))
+        if len(fields) < number_of_fields:
+            num_blank_fields_to_add = number_of_fields - len(fields)
+            if debug:
+                print(f"Adding {num_blank_fields_to_add} empty fields")
+            fields += [''] * num_blank_fields_to_add
         # Wrap extra fields into the last field
-        fields[-1] = separator.join(fields[6:])
-        # print(f"Fields:\n\n{fields}")
-        wja_str, genotype, phenotype, source, description, additional_comments = fields
+        elif len(fields) > number_of_fields:
+            extra_fields = fields[number_of_fields-1:]
+            fields = fields[:number_of_fields]
+            if debug:
+                print(f"Extra fields: {extra_fields}")
+                print(f"Non-extra fields: {fields}")
+            fields[-1] = ' | '.join(extra_fields)
+            if debug:
+                print(f"Fields after wrapping extra fields: {fields}")
+            
+        # Make a dictionary of the whole thing
+        fields_dict = dict(zip(field_names, fields))
+        if debug:
+            pprint(fields_dict)
+        
         # Convert wja to integer, removing 'WJA' prefix and leading zeros
-        wja_str = wja_str.strip().lstrip('WJA').lstrip('0')
+        fields_dict = {k: v.strip() for k, v in fields_dict.items()}
+        fields_dict['wja'] = fields_dict.pop('wja', '').upper().lstrip('WJA').lstrip('0')
         try:
-            wja = int(wja_str)
+            fields_dict['wja'] = int(fields_dict['wja'])
         except ValueError:
-            wja = None  # Invalid WJA number
-        strain_data = {
-            'wja': wja,
-            'genotype': genotype.strip(),
-            'phenotype': phenotype.strip(),
-            'source': source.strip(),
-            'description': description.strip(),
-            'additional_comments': additional_comments.strip(),
-        }
-        # pprint(strain_data)
+            fields_dict['wja'] = None  # Invalid WJA number
+        strain_data = fields_dict
         parsed_data.append(strain_data)
     return parsed_data
