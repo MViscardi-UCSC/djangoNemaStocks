b29d70b83b4a46dd1dfb138bd630027d7a9c7270
MViscardi-UCSC <mviscard@ucsc.edu>
2024-11-01 13:44:30 -0700

    Getting rid of old URLs

--- djangoNemaStocks/urls.py
+++ djangoNemaStocks/urls.py
@@ -50,14 +50,9 @@ urlpatterns = [
     path('new_strain/', nema_views.new_strain, name='new_strain'),
     path('edit_strain/<int:wja>/', nema_views.edit_strain, name='edit_strain'),
     path('strain_details/<int:wja>/', nema_views.strain_details, name='strain_details'),
-    #BULK:
+    # BULK:
     path('bulk_upload_strains/', nema_views.bulk_upload_strains, name='bulk_upload_strains'),
     path('bulk_confirm_strains/', nema_views.bulk_confirm_strains, name='bulk_confirm_strains'),
-    # Freeze Groups:
-    # path('freeze_group_list/', nema_views.freeze_group_list, name='freeze_group_list'),
-    # path('new_freeze_group/', nema_views.new_freeze_group, name='new_freeze_group'),
-    # path('edit_freeze_group/<int:freeze_group_id>/', nema_views.edit_freeze_group, name='edit_freeze_group'),
-    # path('freeze_group_details/<int:freeze_group_id>/', nema_views.freeze_group_details, name='freeze_group_details'),
     # Make Requests:
     path('freeze_request_form/', nema_views.freeze_request_form, name='freeze_request_form'),
     path('freeze_request_form/confirmation/', nema_views.freeze_request_confirmation, name='freeze_request_confirmation'),
@@ -67,8 +62,6 @@ urlpatterns = [
     path('outstanding_freeze_requests/', nema_views.outstanding_freeze_requests, name='outstanding_freeze_requests'),
     path('ongoing_freezes/', nema_views.ongoing_freezes, name='ongoing_freezes'),
     path('ongoing_thaws/', nema_views.ongoing_thaws, name='ongoing_thaws'),
-    # path('freeze_request_management/requests:<str:freeze_request_ids>/', nema_views.freeze_request_management,
-    #      name='freeze_request_management'),
     path('freeze_request_change_confirmation/action=<str:action>/requests=<str:freeze_request_ids>/',
          nema_views.freeze_request_change_confirmation, name='freeze_request_change_confirmation'),
     path('thaw_request_change_confirmation/action=<str:action>/requests=<str:thaw_request_ids>/',
