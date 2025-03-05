2f4ece9c3923fac491435f623f89f30c400fcd17
MViscardi-UCSC <mviscard@ucsc.edu>
2025-03-05 08:24:32 -0800

    add heroku cli

---
+++ Procfile
@@ -0,0 +1,1 @@
+ web: gunicorn djangoNemaStocks.wsgi
--- djangoNemaStocks/settings.py
+++ djangoNemaStocks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
+import django_heroku
 
 import environ
 
@@ -18,6 +19,8 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+django_heroku.settings(locals())
 
 # Adding a static root for deployment based on:
 # https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/#static-root-and-static-url
